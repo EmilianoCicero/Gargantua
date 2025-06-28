055b612ee673b457fd9061adc84a3c6aae84b7b6
Emi <emiliano@Emis-Mac-mini.local>
2025-06-28 19:14:04 +0200

    update to check dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Button from './components/Button';
 import Container from './layouts/Container';
-import './styles/tokens.css';
 import Card from './components/Card';
+import './styles/tokens.css';
 
 function App() {
   const [theme, setTheme] = useState('light');
